deda20bb66268dc0d67964301ff951629304e5ec
Lukas Gross <lu851gro@htwg-konstanz.de>
2018-04-25 11:26:29 +0200

    added more vocabulary tests

--- WordAnalysis/src/test/scala/de/htwg/kiseminar/VocabularyTest.scala
+++ WordAnalysis/src/test/scala/de/htwg/kiseminar/VocabularyTest.scala
@@ -3,7 +3,6 @@ package de.htwg.kiseminar
 import java.io.{File, FileInputStream}
 
 import org.deeplearning4j.models.word2vec.Word2Vec
-import org.deeplearning4j.models.word2vec.wordstore.VocabularyHolder
 import org.deeplearning4j.text.sentenceiterator.BasicLineIterator
 import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor
 import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory
@@ -11,7 +10,7 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class VocabularyTest extends FlatSpec with Matchers {
 
-  it should "add words to vocabulary" in {
+  it should "add words to vocabulary in lower case" in {
     val fileInputStream = new FileInputStream(new File("src/test/resources/sample_text.txt"))
 
     val iter = new BasicLineIterator(fileInputStream)
@@ -24,21 +23,62 @@ class VocabularyTest extends FlatSpec with Matchers {
     t.setTokenPreProcessor(new CommonPreprocessor)
     val vec: Word2Vec = new Word2Vec.Builder()
       .minWordFrequency(1)
-      .iterations(4)
-      .layerSize(100)
-      .seed(42)
-      .windowSize(10)
-      .epochs(5)
       .iterate(iter)
       .tokenizerFactory(t)
       .build
     vec.buildVocab()
 
     val vocab = vec.getVocab
-    println(vocab.vocabWords())
+    vocab.containsWord("ein") should be (true)
+    vocab.containsWord("hund") should be (true)
+    vocab.containsWord("ist") should be (true)
+    vocab.containsWord("tier") should be (true)
   }
 
-  it should "increment count for word 'Test' in vocabulary" in {
+  it should "increment count for word 'ein' in vocabulary" in {
+    val fileInputStream = new FileInputStream(new File("src/test/resources/sample_text.txt"))
 
+    val iter = new BasicLineIterator(fileInputStream)
+    // Split on white spaces in the line to get words
+    val t = new DefaultTokenizerFactory
+    /* CommonPreprocessor will apply the following regex to each token: [\d\.:,"'\(\)\[\]|/?!;]+
+      So, effectively all numbers, punctuation symbols and some special symbols are stripped off.
+      Additionally it forces lower case for all tokens.
+     */
+    t.setTokenPreProcessor(new CommonPreprocessor)
+    val vec: Word2Vec = new Word2Vec.Builder()
+      .minWordFrequency(1)
+      .iterate(iter)
+      .tokenizerFactory(t)
+      .build
+    vec.buildVocab()
+
+    val vocab = vec.getVocab
+    vocab.wordFrequency("ein") should be (2)
+  }
+
+  it should "not contains numbers and punctuation marks" in {
+    val fileInputStream = new FileInputStream(new File("src/test/resources/sample_text.txt"))
+
+    val iter = new BasicLineIterator(fileInputStream)
+    // Split on white spaces in the line to get words
+    val t = new DefaultTokenizerFactory
+    /* CommonPreprocessor will apply the following regex to each token: [\d\.:,"'\(\)\[\]|/?!;]+
+      So, effectively all numbers, punctuation symbols and some special symbols are stripped off.
+      Additionally it forces lower case for all tokens.
+     */
+    t.setTokenPreProcessor(new CommonPreprocessor)
+    val vec: Word2Vec = new Word2Vec.Builder()
+      .minWordFrequency(1)
+      .iterate(iter)
+      .tokenizerFactory(t)
+      .build
+    vec.buildVocab()
+    val vocab = vec.getVocab
+
+    vocab.containsWord(".") should be (false)
+    vocab.containsWord(",") should be (false)
+    vocab.containsWord(")") should be (false)
+    vocab.containsWord("1") should be (false)
   }
 }
